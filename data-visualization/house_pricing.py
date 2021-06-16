d4259787a8b878e8dc2e63892eef4547d88d6e67
Aymeric SIMORRE <aymericsimorre@gmail.com>
2021-06-16 17:41:05 +0200

    Jupyter + house datasets

---
+++ data-visualization/house_pricing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import seaborn as sb
+import matplotlib as mpl
+import numpy as np
+
+# 2) Donnez la liste des variables présentes dans ce dataset ainsi que leur nature (sont elles
+# qualitatives, quantitatives, discrète etc…) et leur type (float, int, str etc…)
+data = pd.read_csv("./data_csv/house_pricing.csv")
+
+data.head()
+
+
+# 3) En créant un nuage de points, regardez comment se comporte la colonne LotArea par
+# rapport au SalesPrices
+
+
+# 4) Affinez votre visualisation en ne gardant uniquement les maisons qui ont un LotArea
+# inférieur à 20 000 pieds carrés et un prix inférieur à 500 000$
+
+
+# 5) En créant un nuage de points, regardez la relation entre le LotFrontage et le LotArea
+
+
+# 6) De la même manière, affinez votre visualisation en ne gardant uniquement les maisons
+# qui ont un LotFrontage inférieur à 200 pieds carrés et un LotArea inférieur à 100000
+# pieds carré
